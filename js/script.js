f775a6487a64a623be17966e1c8ef070a7351b45
vramos29 <vramos1@northpark.edu>
2026-03-29 15:27:28 -0500

    added api key/link, added event lsitener for button click. testing soon

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("script.js loaded");
+
+async function getgif() {
+  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=A4DgvoQDt51sLdOPcbLwrHuYG7l3KaPK&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+  const data = await response.json();
+  const gif = data.gif[0];
+  
+  const output = document.getElementById("gif-container"); 
+  output.textContent = gif;
+}
+
+
+const button = document.getElementById("fetch-gif-btn"); 
+
+button.addEventListener("click", function () {
+    console.log("button clicked");
+    getgif(); 
+});
