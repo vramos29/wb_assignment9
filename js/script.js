0e3133ea19e5ff6e14a934cea81530f307d0ea84
vramos29 <vramos1@northpark.edu>
2026-03-29 17:14:16 -0500

    fixed code, used documentation to learn new function .map(), gifs successfully load onto page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 console.log("script.js loaded");
 
-async function getgif() {
-  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=A4DgvoQDt51sLdOPcbLwrHuYG7l3KaPK&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
-  const data = await response.json();
-  const gif = data.gif[0];
-  
-  const output = document.getElementById("gif-container"); 
-  output.textContent = gif;
-}
+let images = [];
 
+const gifContainer = document.querySelector('#gif-container')
+const button = document.querySelector('#fetch-gif-btn');
+button.addEventListener("click", async function () { //Event listener to track when button is clicked
+    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=A4DgvoQDt51sLdOPcbLwrHuYG7l3KaPK&q=dogs&limit=25&rating=g");
+    const data = await response.json();
 
-const button = document.getElementById("fetch-gif-btn"); 
+    images = data.data.map(gif => gif.images.original.url); //learned from documentation, it takes the raw array and turns it into a new one to be used
+    //above code goes through the data.data with the .map() function to get a new url, then puts it inside the global images array
 
-button.addEventListener("click", function () {
-    console.log("button clicked");
-    getgif(); 
+    gifContainer.innerHTML = "";
+
+    for (let i = 0; i < images.length; i++) { //incrementation of produced gifs stored
+        gifContainer.innerHTML += `<img src=${images[i]} class = "col-3 mb-3">`;;
+    }
+    //above code loops through array to produce as many gifs as were stored (25); uses 'i' to show the gifs incremented
 });
